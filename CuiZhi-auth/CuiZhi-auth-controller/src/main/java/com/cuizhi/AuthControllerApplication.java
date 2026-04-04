5f71634a2139f8625a826a170e805183a90aa673
PaperMan <2193250347@qq.com>
2026-04-04 22:24:35 +0800

    初始化项目工程 - auth 用户认证模块 - user 用户/团队信息模块 - resource 资源管理模块 - learning 学习计划模块 - agent ai服务模块 - gateway 网关 - monitor 系统监控模块 - common 通用配置/类模块

---
+++ CuiZhi-auth/CuiZhi-auth-controller/src/main/java/com/cuizhi/AuthControllerApplication.java
@@ -0,0 +1,15 @@
+package com.cuizhi;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.cuizhi.mapper")
+@SpringBootApplication
+public class AuthControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthControllerApplication.class, args);
+    }
+
+}
