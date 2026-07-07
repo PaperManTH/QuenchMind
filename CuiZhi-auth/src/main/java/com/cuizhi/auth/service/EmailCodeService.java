19da159955d35e6ed602c23e8ba66c9898023b21
PaperMan <2193250347@qq.com>
2026-07-07 16:57:18 +0800

    更新目录结构，搭建Resource模块基础框架

--- CuiZhi-auth/src/main/java/com/cuizhi/auth/service/EmailCodeService.java
+++
@@ -1,9 +0,0 @@
-package com.cuizhi.auth.service;
-
-public interface EmailCodeService {
-
-    void sendLoginCode(String email);
-
-    boolean verifyLoginCode(String email, String code);
-}
-
--- CuiZhi-auth/src/main/java/com/cuizhi/auth/service/WechatUserBindService.java
+++
@@ -1,9 +0,0 @@
-package com.cuizhi.auth.service;
-
-import com.cuizhi.auth.model.po.CzUser;
-
-public interface WechatUserBindService {
-
-    CzUser upsertByWechat(String openid, String unionid, String nickname, String avatar);
-}
-
